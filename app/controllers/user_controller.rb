6297af68dfd7dec585f0969b8788332905a9ed52
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-10-30 15:23:56 +0530

    create new, show and create for user

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,22 @@
+class UserController < ApplicationController
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+
+end

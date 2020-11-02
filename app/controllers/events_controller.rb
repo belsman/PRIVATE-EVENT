f96a927a56f1339bba4625df317ab95c20efa480
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-02 11:53:57 +0100

    Implement Event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
-
+  include EventsHelper
+  
   # GET /events
   # GET /events.json
   def index
@@ -63,14 +64,4 @@ class EventsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_event
-      @event = Event.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def event_params
-      params.fetch(:event, {})
-    end
 end

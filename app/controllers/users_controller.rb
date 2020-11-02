f0e37c958e65e251fd2945d235b41859e7926f2d
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-02 16:49:06 +0100

    Implement the past events and present event feature

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_events = @user.created_events
+    @attended_events = @user.attended_events
   end
 
 

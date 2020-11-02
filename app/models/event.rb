e37efd40ea4c841f279e8491d4809bc5e8d5f634
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-11-02 18:40:13 +0530

    Add table to link User, event and event attendees

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
+  has_many :attendee, through: :event_attendees
+  has_many :event_attendees, foreign_key: :attendee_id
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_30_134828) do
+ActiveRecord::Schema.define(version: 2020_11_02_125346) do
+
+  create_table "event_attendees", force: :cascade do |t|
+  end
 
   create_table "events", force: :cascade do |t|
     t.datetime "starts_at"

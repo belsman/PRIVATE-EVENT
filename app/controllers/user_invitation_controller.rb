98954e84f9d6ed9f49f3d2cc180d2fbde8b96259
Bello S Babakolo <fleetbeekay@gmail.com>
2020-11-03 16:49:22 +0100

    Implement Invitation#creation

--- app/controllers/user_invitation_controller.rb
+++ app/controllers/user_invitation_controller.rb
@@ -1,7 +1,15 @@
 class UserInvitationController < ApplicationController
 
   def create
-    flash[:notice] = "Invitation sent successfully"
+    event_id = params[:event_id]
+    user = User.where(name: params[:name]).first
+    if user
+      user_id = user.id
+      event = EventAttendee.new(attendee_id: user_id, attended_event_id: event_id)
+      event.save
+      flash[:notice] = "Invitation sent successfully"
+    end
+    redirect_to event_path(event_id)
   end
   
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
-  has_many :event_attendees, foreign_key: :attendee_id
-  has_many :attendees, through: :event_attendees, source: :attendee
+  has_many :event_attendees, foreign_key: :attended_event_id
+  has_many :attendees, through: :event_attendees, class_name: "User"
 
   scope :future_events, -> { where('starts_at > ?', Time.now) }
   scope :past_events, -> { where('starts_at <= ?', Time.now) }

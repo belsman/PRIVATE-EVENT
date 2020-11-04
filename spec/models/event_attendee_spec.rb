ea620477f45c9de320f6458f3570b791243a1b94
Abhiyan001 <abhigyanmahanta001@gmail.com>
2020-11-04 18:36:31 +0530

    Initialise test cases

--- spec/models/event_attendee_spec.rb
+++ spec/models/event_attendee_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe EventAttendee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    context "Checking event attendee" do
+        it 'returns a user as the creator of the Event' do
+            user = User.create(name: "Captain Alex")
+            event = Event.create(name: "Party #1", description: "Don't call your mama", creator_id: user.id, starts_at: Time.now)
+            expect(event.creator).to eq(user)
+        end
+
+        it 'throws an error when an #talk is invoked' do
+            user = User.create(name: "Captain Alex")
+            event = Event.create(name: "Party #1", description: "Don't call your mama", creator_id: user.id, starts_at: Time.now)
+            expect { user.talk }.to raise_error(NoMethodError)
+        end
+    end
 end
